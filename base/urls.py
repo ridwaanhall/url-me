5c2dbe1d3b693b1d35d1b04b3f4e96917a916227
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-27 07:17:13 +0000

    modified urls

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,7 @@
 # base/urls.py
 from django.urls import path
-from .views import hello_world
+from . import views
 
 urlpatterns = [
-    path('', hello_world, name='hello_world'),
+    path('', views.hello_world, name='hello_world'),
 ]
