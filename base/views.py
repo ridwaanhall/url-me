fc3f50f9b9d63fdc33c90069c4755b4753bbc594
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-12-28 06:31:35 +0000

    added function of redirect custom path

--- base/views.py
+++ base/views.py
@@ -1,6 +1,12 @@
 # base/views.py
-from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import ShortenedURL
+
 
 def hello_world(request):
     return render(request, 'base/form.html')
+
+def redirect_custom_path(request, custom_path):
+    shortened_url = get_object_or_404(ShortenedURL, custom_path=custom_path)
+    return redirect(shortened_url.long_url)
