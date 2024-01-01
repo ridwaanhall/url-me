5d59a5d2b6248a4e0e3ea546a5d153d00f663735
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2024-01-01 04:18:54 +0000

    updated forms

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,3 @@
-# forms.py
 from django import forms
 from .models import ShortenedURL
 
@@ -6,3 +5,12 @@ class ShortenedURLForm(forms.ModelForm):
     class Meta:
         model = ShortenedURL
         fields = ['long_url', 'custom_domain', 'custom_path']
+
+    def clean_custom_path(self):
+        custom_path = self.cleaned_data.get('custom_path')
+
+        # Check if the custom path already exists in the database
+        if ShortenedURL.objects.filter(custom_path=custom_path).exists():
+            raise forms.ValidationError('Custom path already exists.')
+
+        return custom_path
