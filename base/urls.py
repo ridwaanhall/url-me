08fb9606732826192d6593825e93871eb1fde180
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2024-01-01 04:34:07 +0000

    formatted python structure

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.form_page, name='form_page'),
-    path('<str:custom_path>/', views.redirect_custom_path, name='redirect_custom_path'),
+    path('<str:custom_path>/',
+         views.redirect_custom_path,
+         name='redirect_custom_path'),
 ]
