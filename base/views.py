5085448a3eca262ef0d10361c942c30cd2420662
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2024-01-01 04:19:41 +0000

    deleted unused code

--- base/views.py
+++ base/views.py
@@ -3,22 +3,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import ShortenedURL
-from django.urls import reverse
 from .forms import ShortenedURLForm
-
-# def form_page(request):
-#     # long_url = models.URLField()
-#     # custom_domain = models.URLField(blank=True, null=True)
-#     # custom_path = models.CharField(max_length=20, unique=True, blank=True, null=True, default=generate_random_path)
-#     if request.method == 'POST':
-#         long_url = request.POST.get('long_url')
-#         custom_domain = request.POST.get('custom_domain')
-#         custom_path = request.POST.get('custom_path')
-
-#         try:
-            
-
-#     return render(request, 'base/form.html', context)
 
 
 def form_page(request):
@@ -36,16 +21,11 @@ def form_page(request):
         form = ShortenedURLForm(request.POST)
         if form.is_valid():
             shortened_url = form.save(commit=False)
-            shortened_url.clean()  # Ensure model clean is executed
-            shortened_url.save()   # Save to the database
+            shortened_url.clean()
+            shortened_url.save()
 
-            # Get the full URL of the created short URL
             full_shortened_url = f'{shortened_url.custom_domain}/{shortened_url.custom_path}'
-
-            # Add a success flash message with the shortened URL
             messages.success(request, f'Shortened URL created successfully! {full_shortened_url}')
-
-            # Redirect or render a success page
             return redirect('form_page')
 
     else:
@@ -53,7 +33,6 @@ def form_page(request):
 
     context = {
         'form': form,
-        # 'full_shortened_url': full_shortened_url,
     }
     return render(request, 'base/form.html', context)
 
